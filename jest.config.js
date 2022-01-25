f7b6a8e8a428bd2087a65857577bb23a8828854a
Ayuba Dauda <mailtoayuba@gmail.com>
2022-01-25 12:00:27 -0800

    Finalizing Jest Unit Testing COnfiguration

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: [
+        "ts",
+        "tsx",
+        "js"
+      ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+      },
+    globals: {
+        "ts-jest": {
+          "tsConfig": "tsconfig.json"
+        }
+      },
+    testMatch: [
+        "**/tests/*-test.ts"
+      ]
+  };
+  
---
+++ src/tests/drone-routes-test.ts
@@ -0,0 +1,11 @@
+
+
+describe("DroneRoutes", ()=>{
+    describe("GET /drones",()=>{
+        it("Should call DroneController.getDrones",async ()=>{
+            expect(2+2).toEqual(4)
+        })
+    })    
+})
+
+
